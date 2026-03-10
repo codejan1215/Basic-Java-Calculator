a2eb8595c7a5c8c7a7d7ce9ecbe8e280347112de
player 0 <naybejanrich15@gmail.com>
2026-03-10 12:22:00 +0800

    Delete Java directory

--- Java/SimpleCalculatorOOP/add.java
+++
@@ -1,8 +0,0 @@
-package Java.SimpleCalculatorOOP;
-
-public class add {
-    
-    void add(input input){
-        System.out.printf("%nResult: %.2f + %.2f = %.2f%n", input.getNum1(), input.getNum2(), input.getNum1() + input.getNum2());
-    }
-}
